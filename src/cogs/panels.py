afe85a85b1266f838b9cab395381341c2ad65c37
Nat1anWasTaken <nathan@nat1an.xyz>
2024-04-21 23:37:17 +0800

    ✨ feat(src/cogs/panels.py, extensions.json): Add new feature for sending specific control panels

---
+++ src/cogs/panels.py
@@ -0,0 +1,39 @@
+import logging
+
+from disnake import ApplicationCommandInteraction, Option, OptionType
+from disnake.ext.commands import Cog, slash_command
+
+from src.bot import Krabbe
+from src.panels import panels
+
+
+class Panels(Cog):
+    def __init__(self, bot: Krabbe):
+        self.bot = bot
+        self.logger = logging.getLogger("krabbe.panels")
+
+    @slash_command(
+        name="panel",
+        description="傳送指定的控制面板",
+        options=[
+            Option(
+                name="panel",
+                description="要傳送的控制面板",
+                type=OptionType.string,
+                choices=[str(key) for key in panels.keys()],
+                required=True
+            )
+        ],
+        guild_ids=[975244147730546758]
+    )
+    async def panel(self, interaction: ApplicationCommandInteraction, panel: str):
+        panel_to_send = panels.get(panel)
+
+        await interaction.channel.send(
+            embed=panel_to_send.embed,
+            view=panel_to_send.view
+        )
+
+
+def setup(bot: Krabbe):
+    bot.add_cog(Panels(bot))
