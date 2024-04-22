2efe5ec08cfb524594166e475d73e0df5fb62566
Nat1anWasTaken <nathan@nat1an.xyz>
2024-04-22 15:20:22 +0800

    ✨ feat(src/classes/voice_channel.py): Add new VoiceChannel class

---
+++ src/classes/voice_channel.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+import disnake
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+from src.bot import Krabbe
+from src.classes.mongo_object import MongoObject
+
+
+class VoiceChannel(MongoObject):
+    collection_name = "voice_channels"
+
+    def __init__(self, database: AsyncIOMotorDatabase, channel_id: int, owner_id: int):
+        super().__init__(database)
+
+        self.channel_id: int = channel_id
+        self.owner_id: int = owner_id
+
+        self._channel: Optional[disnake.VoiceChannel] = None
+        self._owner: Optional[disnake.Member] = None
+
+    def unique_identifier(self) -> dict:
+        return {"channel_id": self.channel_id}
+
+    def to_dict(self) -> dict:
+        return {
+            "channel_id": self.channel_id,
+            "owner_id": self.owner_id
+        }
+
+    @property
+    def channel(self):
+        if self._channel is None:
+            raise ValueError("Channel is not resolved yet. Consider calling the resolve method.")
+        return self._channel
+
+    @property
+    def owner(self):
+        if self._owner is None:
+            raise ValueError("Owner is not resolved yet. Consider calling the resolve method.")
+        return self._owner
+
+    async def resolve(self, bot: Krabbe) -> "VoiceChannel":
+        """
+        Resolves the channel and owner objects.
+
+        :param bot: The Krabbe bot instance.
+        :return: The resolved VoiceChannel object.
+        """
+        self._channel = bot.get_channel(self.channel_id)
+        self._owner = self.channel.guild.get_member(self.owner_id)
+
+        return self
