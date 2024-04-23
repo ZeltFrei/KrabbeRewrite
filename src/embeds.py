811629c569b79f4b3ceedc1f0b1bb3c0c6afb3e5
Nat1anWasTaken <nathan@nat1an.xyz>
2024-04-23 17:07:27 +0800

    ✨ feat(src/embeds.py): Add new custom embed classes for handling bot messages

---
+++ src/embeds.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from disnake import Embed, Color
+
+
+class ErrorEmbed(Embed):
+    """
+    The embed used when errors
+    """
+
+    def __init__(self, title: str, description: Optional[str]):
+        super().__init__(title=f"❌ | {title}", description=description, color=Color.red())
+
+
+class WarningEmbed(Embed):
+    """
+    The embed used when bot needs to warn something
+    """
+
+    def __init__(self, title: str, description: Optional[str]):
+        super().__init__(title=f"⚠️ | {title}", description=description, color=Color.yellow())
+
+
+class LoadingEmbed(Embed):
+    """
+    The embed used when bot is loading something
+    """
+
+    def __init__(self, title: str, description: Optional[str]):
+        super().__init__(title=f"⏳ | {title}", description=description, color=Color.blue())
+
+
+class SuccessEmbed(Embed):
+    """
+    The embed used when successful
+    """
+
+    def __init__(self, title: str, description: Optional[str]):
+        super().__init__(title=f"✅ | {title}", description=description, color=Color.green())
+
+
+class NotifyEmbed(Embed):
+    """
+    The embed used when notifying something
+    """
+
+    def __init__(self, title: str, description: Optional[str]):
+        super().__init__(title=f"ℹ️ | {title}", description=description, color=Color.blurple())
