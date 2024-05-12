77daf20d7e656ab2ec2a8928c6ac5fa6b792c6ce
Nat1anWasTaken <nathan@nat1an.xyz>
2024-05-12 13:28:44 +0800

    feat(src/kava/utils.py): add functions for music permissions and client validation

--- src/kava/utils.py
+++ src/kava/utils.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
-from disnake import Guild
+from disnake import Guild, Interaction
 
 from src.classes.voice_channel import VoiceChannel
+from src.embeds import ErrorEmbed
 from src.kava.server import KavaServer, ServerSideClient
 
 
@@ -34,9 +35,9 @@ def get_idle_clients_in(server: KavaServer, guild: Guild) -> list[ServerSideClie
     ]
 
 
-def get_playing_client_in(server: KavaServer, channel: VoiceChannel) -> Optional[ServerSideClient]:
+def get_active_clients_in(server: KavaServer, channel: VoiceChannel) -> Optional[ServerSideClient]:
     """
-    Get the playing client in the guild.
+    Get the active client in the channel.
 
     :param server: The server to get the clients from.
     :param channel: The channel to get the playing client in.
@@ -47,3 +48,67 @@ def get_playing_client_in(server: KavaServer, channel: VoiceChannel) -> Optional
             return server.clients[member.id]
 
     return None
+
+
+def has_music_permissions(user_id: int, channel: VoiceChannel) -> bool:
+    """
+    Check if the user has music permissions in the channel.
+
+    :param user_id: The user ID to check.
+    :param channel: The channel to check.
+    :return: True if the user has music permissions. False otherwise.
+    """
+    return channel.owner_id == user_id  # TODO: integrate with channel settings
+
+
+async def ensure_music_permissions(interaction: Interaction) -> Optional[VoiceChannel]:
+    """
+    This check ensures that the user is allowed to use the music commands.
+    :param interaction: The interaction that triggered the command.
+    :return: The active channel in the interaction.
+    """
+    channel = VoiceChannel.get_active_channel_from_interaction(interaction)
+
+    if not has_music_permissions(interaction.author.id, channel):  # TODO: integrate with channel settings
+        await interaction.response.send_message(
+            embed=ErrorEmbed("你不是這個頻道的擁有者！"),
+            ephemeral=True
+        )
+        return None
+
+    return channel
+
+
+async def ensure_music_client(server: KavaServer, interaction: Interaction) -> Optional[ServerSideClient]:
+    """
+    This check ensures that there's a active client in the channel.
+    :param server: The server to get the clients from.
+    :param interaction: The interaction that triggered the command.
+    :return: The active client in the channel. None if no active client is found.
+    """
+    if not interaction.guild:
+        await interaction.response.send_message(
+            embed=ErrorEmbed("這個指令只能在伺服器中使用。"),
+            ephemeral=True
+        )
+        return
+
+    channel = VoiceChannel.get_active_channel_from_interaction(interaction)
+
+    if not channel:
+        await interaction.response.send_message(
+            embed=ErrorEmbed("請先加入一個語音頻道。"),
+            ephemeral=True
+        )
+        return
+
+    client = get_active_clients_in(server, channel)
+
+    if client is None:
+        await interaction.response.send_message(
+            embed=ErrorEmbed("沒有正在你頻道裡的音樂機器人！"),
+            ephemeral=True
+        )
+        return
+
+    return client
