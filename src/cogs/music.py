fbaf540935596732e3bed1a71256a5fddf830b7d
Nat1anWasTaken <nathan@nat1an.xyz>
2024-06-12 11:16:24 +0800

    feat(src/cogs/music.py): add radio command to play Krabbe 2.0 station content, with warning about clearing the current playlist

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -68,9 +68,15 @@ class Music(Cog):
 
     @slash_command(
         name="radio",
-        description="將ZeitFrei 電台的內容加入播放序列"
+        description="將 Krabbe 2.0 電台的內容加入播放序列 (⚠️ 這個指令會清空目前的播放序列)"
     )
     async def radio(self, interaction: ApplicationCommandInteraction):
+        if not (channel := await ensure_music_permissions(interaction)):
+            return
+
+        if client := get_active_client_in(self.bot.kava_server, channel):
+            await client.request("clean", channel_id=channel.channel_id)
+
         await self.play_command(
             interaction, query="https://www.youtube.com/playlist?list=PL5WxzmH3aonl25d6gv48o1ByFmy05RBSR"
         )
